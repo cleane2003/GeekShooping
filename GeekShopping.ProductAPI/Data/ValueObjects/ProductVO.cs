6a76fc230ee9323eaa586551abc17ba4100582d9
Cleane Batista <cleaneevelin04@gmail.com>
2026-03-05 16:17:08 -0300

    feat: Definindo layout e consumindo o microsserviço de produtos

--- GeekShopping.ProductAPI/Data/ValueObjects/ProductVO.cs
+++ GeekShopping.ProductAPI/Data/ValueObjects/ProductVO.cs
@@ -1,15 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace GeekShopping.ProductAPI.Data.ValueObjects
+﻿namespace GeekShopping.ProductAPI.Data.ValueObjects
 {
     public class ProductVO
     {
         public long Id { get; set; }
-        public string Name { get; set; } = string.Empty;
+        public string Name { get; set; }
         public decimal Price { get; set; }
-        public string Description { get; set; } = string.Empty;
-        public string CategoryName { get; set; } = string.Empty;
-        public string ImageUrl { get; set; } = string.Empty;
+        public string Description { get; set; }
+        public string CategoryName { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
--- GeekShopping.Web/Models/ProductModel.cs
+++ GeekShopping.Web/Models/ProductModel.cs
@@ -7,6 +7,6 @@
         public decimal Price { get; set; }
         public string Description { get; set; } = string.Empty;
         public string CategoryName { get; set; } = string.Empty;
-        public string ImageUrl { get; set; } = string.Empty;
+        public string ImageURL { get; set; } = string.Empty;
     }
 }
