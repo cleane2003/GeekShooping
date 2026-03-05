16c7e73746da9660fa0309998c484363124decc1
Cleane Batista <cleaneevelin04@gmail.com>
2026-03-05 13:59:19 -0300

    feat: preparando microserviço web para consumo de produtos

--- GeekShopping.Web/Program.cs
+++ GeekShopping.Web/Program.cs
@@ -1,7 +1,17 @@
+using GeekShopping.Web.Services;
+using GeekShopping.Web.Services.IServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var productApiUrl = builder.Configuration["ServiceUrls:ProductAPI"]
+    ?? throw new InvalidOperationException("ServiceUrls:ProductAPI não está configurado");
+
 // Add services to the container.
+
+
 builder.Services.AddControllersWithViews();
+builder.Services.AddHttpClient<IProductService, ProductService>(c =>
+    c.BaseAddress = new Uri(productApiUrl));
 
 var app = builder.Build();
 
@@ -10,6 +20,7 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+
 app.UseStaticFiles();
 
 app.UseRouting();
---
+++ GeekShopping.Web/Services/ProductService.cs
@@ -0,0 +1,57 @@
+﻿using GeekShopping.Web.Models;
+using GeekShopping.Web.Services.IServices;
+using GeekShopping.Web.Utils;
+
+namespace GeekShopping.Web.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly HttpClient _client;
+        public const string BasePath = "api/v1/product";
+
+
+        public ProductService(HttpClient client)
+        {
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+        }
+        public Task<ProductModel> CreateProduct(ProductModel model)
+        {
+            var response = _client.PostAsJsonAsync(BasePath, model);
+
+            if (response.Result.IsSuccessStatusCode)
+                return response.Result.ReadContentAs<ProductModel>();
+            else throw new ApplicationException($"Something went wrong calling the API: {response.Result.ReasonPhrase}");
+        }
+
+        public async Task<IEnumerable<ProductModel>> FindAllProducts()
+        {
+            var response = await _client.GetAsync(BasePath);
+
+            return await response.ReadContentAs<List<ProductModel>>();
+        }
+
+        public async Task<ProductModel> FindProductById(long id)
+        {
+            var response = await _client.GetAsync($"{BasePath}/{id}");
+
+            return await response.ReadContentAs<ProductModel>();
+        }
+
+        public Task<ProductModel> UpdateProduct(ProductModel model)
+        {
+            var response = _client.PutAsJson(BasePath, model);
+
+            if (response.Result.IsSuccessStatusCode)
+                return response.Result.ReadContentAs<ProductModel>();
+            else throw new ApplicationException($"Something went wrong calling the API: {response.Result.ReasonPhrase}");
+        }
+
+        public async Task<bool> DeleteProductById(long id)
+        {
+            var response = await _client.DeleteAsync($"{BasePath}/{id}");
+            if (response.IsSuccessStatusCode)
+                return await response.ReadContentAs<bool>();
+            else throw new ApplicationException($"Something went wrong calling the API: {response.ReasonPhrase}");
+        }
+    }
+}
